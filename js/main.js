f872e46a11f5c6245fe0cec391314a3883e46ef7
Jmb2602 <jmontalvob11@gmail.com>
2025-02-13 14:36:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Tu Juli Te Ama').split('')
+    const titles = ('Tu Juli Te Ama por Siempre').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
