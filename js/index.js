5184d32f070486ca3b91bd0696dd434af966158c
Jmb2602 <jmontalvob11@gmail.com>
2025-02-13 14:35:41 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Una Sorpresa Osita'.split('')
+const text = 'Lo Encontraste Osita'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
